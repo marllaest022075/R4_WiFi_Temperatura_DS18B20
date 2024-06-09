0cafffef84864ce908fb2306b555dddd528e2325
marllaest022075 <marllaest2075@hotmail.com>
2024-06-09 14:38:19 -0700

    Read
    Temperature from pin 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,108 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Data wire is plugged into port 2 on the Arduino
+#define ONE_WIRE_BUS 3
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+
+// arrays to hold device address
+DeviceAddress insideThermometer;
+
+// Display
+LiquidCrystal_I2C display(0x27, 16, 2);
+
+void InitDisplay(void);
+void printAddress(DeviceAddress deviceAddress);
+void printTemperature(DeviceAddress deviceAddress);
+
+/*
+ * Setup function. Here we do the basics
+ */
+void setup(void)
+{
+  // start serial port
+  Serial.begin(9600);
+  Serial.println("Dallas Temperature IC Control Library Demo");
+  InitDisplay();
+  pinMode(7, OUTPUT);
+  digitalWrite(7, HIGH);
+
+  // locate devices on the bus
+  Serial.print("Locating devices...");
+  sensors.begin();
+  Serial.print("Found ");
+  Serial.print(sensors.getDeviceCount(), DEC);
+  Serial.println(" devices.");
+
+  // report parasite power requirements
+  Serial.print("Parasite power is: ");
+  if (sensors.isParasitePowerMode())
+    Serial.println("ON");
+  else
+    Serial.println("OFF");
+
+  if (!sensors.getAddress(insideThermometer, 0))
+    Serial.println("Unable to find address for Device 0");
+
+  // show the addresses we found on the bus
+  Serial.print("Device 0 Address: ");
+  printAddress(insideThermometer);
+  Serial.println();
+
+  // set the resolution to 9 bit (Each Dallas/Maxim device is capable of several different resolutions)
+  sensors.setResolution(insideThermometer, 9);
+
+  Serial.print("Device 0 Resolution: ");
+  Serial.print(sensors.getResolution(insideThermometer), DEC);
+  Serial.println();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(void)
+{
+  sensors.requestTemperatures();
+  printTemperature(insideThermometer);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void printTemperature(DeviceAddress deviceAddress)
+{
+  float tempC = sensors.getTempC(deviceAddress);
+  if (tempC == DEVICE_DISCONNECTED_C)
+  {
+    Serial.println("Error: Could not read temperature data");
+    display.setCursor(0, 0);
+    display.print("Error: Could not");
+    display.setCursor(0, 1);
+    display.print("read temperature data ");
+    return;
+  }
+  Serial.print("Temp C: ");
+  Serial.println(tempC);
+  display.setCursor(0, 0);
+  display.print("Temp C: ");
+  display.print(tempC);
+}
+
+void InitDisplay()
+{
+  display.init();
+  display.clear();
+  display.backlight();
+}
+
+void printAddress(DeviceAddress deviceAddress)
+{
+  for (uint8_t i = 0; i < 8; i++)
+  {
+    if (deviceAddress[i] < 16)
+      Serial.print("0");
+    Serial.print(deviceAddress[i], HEX);
+  }
 }
